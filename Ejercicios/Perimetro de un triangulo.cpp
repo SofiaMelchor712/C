ce20c89529dcb752756a07c1442e23920e9ee5b5
SofiaMelchor712 <71915462+SofiaMelchor712@users.noreply.github.com>
2020-10-23 17:39:59 -0500

    Tarea . Perimetro_triangulo
    tarea

--- Ejercicios/Perimetro de un triangulo.cpp
+++ Ejercicios/Perimetro de un triangulo.cpp
@@ -3,13 +3,13 @@
 int main()
 {
 	
-float l, p;
+float l, perimetro;
 
 printf("introduce el lado de un triangulo equilatero:");
 scanf("%f",&l);
 
-p=l+l+l;
-printf("El perimetro del triangulo equilatero es:%0.2f",p);
+perimetro=l+l+l;
+printf("El perimetro del triangulo equilatero es:%0.2f",perimetro);
 
 return 0;
 }
